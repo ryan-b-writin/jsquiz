68b14f837f3d51ab30a038a181e175b8cf3fe7cd
Ryan B <ryan.bliton@gmail.com>
2016-02-15 22:02:49 -0600

    cleaned up code a little

--- scripts.js
+++ scripts.js
@@ -10,20 +10,19 @@ button.addEventListener("click",function(event) {
     height: document.getElementById("height").value,
     letter: document.getElementById("tree").value
   };
-  var tall = parseInt(treeObject.height);
-  var numLevels = (parseInt(tall) + 1 ) / 2;
-  var treeFill = " "
+  var heightInt = parseInt(treeObject.height);
+  var numLevels = (heightInt + 1 ) / 2;
+  var treeFill = ""
 
 // loop through to add characters to variable every level
-  for (i=0; i<tall; i++) {
+  for (i=0; i<heightInt; i++) {
   treeFill += treeObject.letter;
-
-
 
 // only log on odds
     if (i % 2 === 0) {
-      var spaces = ""
+
 //subtract 1 from numLevels and add that many spaces
+      var spaces = ""
       numLevels -= 1
       for (j=0;j<numLevels;j++) {
       spaces += " "
