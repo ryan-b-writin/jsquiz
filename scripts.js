c37e048591f2b4ca25d46d0c023c887b551a2d1d
Ryan B <ryan.bliton@gmail.com>
2016-02-15 22:07:21 -0600

    just had to clean up some more

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ var button = document.getElementById("btn");
 // click event
 button.addEventListener("click",function(event) {
 
-// tree object
+  // tree object
   var treeObject = { 
     height: document.getElementById("height").value,
     letter: document.getElementById("tree").value
@@ -13,21 +13,19 @@ button.addEventListener("click",function(event) {
   var heightInt = parseInt(treeObject.height);
   var numLevels = (heightInt + 1 ) / 2;
   var treeFill = ""
-
-// loop through to add characters to variable every level
+  // loop through to add characters to variable every level
   for (i=0; i<heightInt; i++) {
-  treeFill += treeObject.letter;
-
-// only log on odds
+    treeFill += treeObject.letter;
+    // only log on odds
     if (i % 2 === 0) {
-
-//subtract 1 from numLevels and add that many spaces
+      //subtract 1 from numLevels and add that many spaces
       var spaces = ""
       numLevels -= 1
       for (j=0;j<numLevels;j++) {
       spaces += " "
     };
-      console.log(spaces,treeFill); 
+    //log that beautiful tree
+    console.log(spaces,treeFill); 
     };
   };
 });
